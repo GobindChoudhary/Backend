51b071a8528f90adeb5d2bde9b91906bd0be60b2
GobindChoudhary <chaudharygobind24@gmail.com>
2025-07-04 13:55:33 +0530

    first server

--- Day 1/app.js
+++ Day 1/app.js
@@ -1,12 +1,12 @@
 const catMe = require("cat-me");
 console.log(catMe());
-const http = require("http");
+const http = require("http"); // importing http module (no need to install pre build)
 
 const server = http.createServer((req, res) => {
   res.write("Hello, Client!");
   res.end("Hello ,world");
-});
+}); // server created
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
-});
+}); //starting the server
