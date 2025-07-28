dddffe6b32a01ae84da8143f46c834832cfe1de3
GobindChoudhary <chaudharygobind24@gmail.com>
2025-07-29 00:59:29 +0530

    day 6

--- Day 5&6/src/DB/db.js
+++ Day 5&6/src/DB/db.js
@@ -3,7 +3,7 @@ const mongooes = require("mongoose");
 async function connectToDB() {
   await mongooes
     .connect(
-      "mongodb+srv://chaudharygobind:helloApnaPassword@cluster0.kzioxt2.mongodb.net/"
+      "mongodb+srv://chaudharygobind24:y55bK2Bh3JjHzuLI@cluster0.kzioxt2.mongodb.net/test/notes"
     )
     .then(() => console.log("Connected to DataBase"));
 }
