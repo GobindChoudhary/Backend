b38b7a60a0d64aee043eda2620810fcbd724d65c
GobindChoudhary <chaudharygobind24@gmail.com>
2025-08-05 12:46:37 +0530

    user router

--- Day 12 captionGenerator/src/controllers/auth.controller.js
+++ Day 12 captionGenerator/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 async function registerController(req, res) {
   const { userName, password } = req.body;
@@ -13,9 +14,10 @@ async function registerController(req, res) {
     });
   }
 
+  const hashPassword = await bcrypt.hash(password, 10);
   const newUser = await userModel.create({
     userName,
-    password,
+    password: hashPassword,
   });
 
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_URL);
@@ -36,8 +38,8 @@ async function loginController(req, res) {
       message: `user with name ${userName} doesn't exist`,
     });
   }
-
-  if (user.password != password) {
+  const isMatched = await bcrypt.compare(password, user.password);
+  if (!isMatched) {
     return res.status(401).json({
       message: "Invalid Password",
     });
@@ -51,6 +53,30 @@ async function loginController(req, res) {
   });
 }
 
-function userController(req, res) {}
+async function userController(req, res) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unautherised access",
+    });
+  }
+
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET_URL);
+
+  if (!decoded) {
+    return res.status(401).json({
+      message: "Unautherised access",
+    });
+  }
+
+  const user = await userModel
+    .findOne({ _id: decoded.id })
+    .select("-password -__v");
+  res.status(200).json({
+    message: "Details fetched successfully",
+    user,
+  });
+}
 
 module.exports = { registerController, loginController, userController };
