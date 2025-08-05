1138090be0c64f2fffe6ccc7d5b5ece981b7baa3
GobindChoudhary <chaudharygobind24@gmail.com>
2025-08-05 17:30:28 +0530

    server created

--- Day 12 captionGenerator/src/app.js
+++ Day 12 captionGenerator/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRoute = require("./routers/auth.routes");
+const postRoute = require("./routers/post.routes");
 const cookieParser = require("cookie-parser");
 const app = express();
 
@@ -7,5 +8,6 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
 
 module.exports = app;
