7650fd7a14d66a18ea05a8409898b449526fda8c
GobindChoudhary <chaudharygobind24@gmail.com>
2025-08-05 12:45:59 +0530

    cookieParser added

---
+++ Day 12 captionGenerator/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const authRoute = require("./routers/auth.routes");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+module.exports = app;
